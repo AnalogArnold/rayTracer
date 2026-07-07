a2c423a89a8c84490d6edb28826aa4a5cc49965d
AnalogArnold <michalvx3@gmail.com>
2026-07-07 13:35:21 +0100

    Restructure

--- raytracer/tests/raytracer/thesis-data/gmsh_pipe.py
+++ raytracer/tests/raytracer/thesis-data/gmsh_pipe.py
@@ -6,9 +6,7 @@ from pathlib import Path
 # IMPORTANT USER INPUTS
 # ============================================================
 
-#OUT_DIR = PARENT_DIR = Path(__file__).resolve().parent / "pipe"
-#OUT_DIR = PARENT_DIR = Path(__file__).resolve().parent / "pipe_shark"
-OUT_DIR = PARENT_DIR = Path(__file__).resolve().parent / "pipe_plate"
+OUT_DIR = PARENT_DIR = Path(__file__).resolve().parent / "pipe"
 
 # ----------------------------
 # Pipe geometry inputs
@@ -17,21 +15,9 @@ OUT_DIR = PARENT_DIR = Path(__file__).resolve().parent / "pipe_plate"
 # For outer cylinder, so general pipe geometry
 y_base = -47.0 # ~ Half of height
 height = 95.0
-# For shark:
-#y_base = - 37
-#height = 74
-# For plate:
-y_base = - 23
-height = 46
 
 diameter_i = 21.1 # mm, ID
 diameter_o = 24.77 # mm, OD
-# For shark:
-#diameter_i = 35
-#diameter_o = 39 # 2mm thick wall
-# For plate:
-diameter_i = 33
-diameter_o = 37
 
 radius_i = diameter_i / 2.0
 radius_o = diameter_o / 2.0
@@ -77,13 +63,13 @@ N_Y    = 80   # number of nodes along the axis (y direction)
 # Smaller values generally mean finer mesh
 
 # Coarse
-DIV_FACTOR = 7.5
+#DIV_FACTOR = 7.5
 
 # Med-fine
 #DIV_FACTOR = 15.0
 
 # Fine
-#DIV_FACTOR = 28.0 # To be comparable to fine in rectangular tank
+DIV_FACTOR = 28.0 # To be comparable to fine in rectangular tank
 
 MESH_SIZE_MIN = min(radius_o, height) / DIV_FACTOR
 MESH_SIZE_MAX = min(radius_o, height) / DIV_FACTOR
